82cee73a17114d5a6be727e96a016b99728d0bd8
pamelanavarroha <pamela.navarroha@gmail.com>
2021-02-07 21:12:53 -0600

    Agregamos tabla de tareas y función creadora de contenedores

--- js/app.js
+++ js/app.js
@@ -54,3 +54,64 @@ divContainerInputAndButton.appendChild(buttonAdd);
 
 divApp.appendChild(divContainerInputAndButton);
 
+            /*Table*/
+            var divContainerTable = document.createElement("div");
+            divContainerTable.id = "containerTable";
+            divContainerTable.className = "container-table";
+
+            var table = document.createElement("table");
+            table.id = "table";
+            table.className = "table";
+
+            divContainerTable.appendChild(table);
+            divApp.appendChild(divContainerTable);
+
+            function createTask(taskValue) {
+                inputNewTask.value = "";
+                var trFila = document.createElement("tr");
+                var tdColumn1 = document.createElement("td");
+
+                var checkBox = document.createElement("INPUT");
+                checkBox.setAttribute("type", "checkbox");
+                checkBox.className = "checkbox";
+                checkBox.addEventListener("click", function () {
+                    var tdParent = checkBox.parentNode;
+                    var trParent = tdParent.parentNode;
+                    if (checkBox.checked) {
+                        trParent.style = "text-decoration: line-through";
+                    } else {
+                        trParent.style = "text-decoration: none";
+                    }
+                });
+
+                tdColumn1.append(checkBox);
+
+                var tdColumn2 = document.createElement("td");
+                var tdColumn3 = document.createElement("td");
+
+                var buttonDelete = document.createElement("BUTTON");
+                buttonDelete.innerHTML = "Delete";
+                buttonDelete.setAttribute("id", "buttonDelete");
+                buttonDelete.setAttribute("class", "button-delete");
+
+                buttonDelete.addEventListener("click", function (event) {
+                    tdParentNodeBtn = event.target.parentNode;
+                    showModal();
+                });
+                tdColumn2.append(taskValue);
+                tdColumn3.append(buttonDelete);
+
+                trFila.appendChild(tdColumn1);
+                trFila.appendChild(tdColumn2);
+                trFila.appendChild(tdColumn3);
+                table.appendChild(trFila);
+                
+            }
+
+            function createContainerElement(idParameter, classParameter) {
+                var div = document.createElement("div");
+                div.setAttribute("id", idParameter);
+                div.setAttribute("class", classParameter);
+                document.getElementById("app").appendChild(div);
+            }
+
