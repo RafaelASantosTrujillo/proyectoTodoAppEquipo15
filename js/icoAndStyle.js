ad7276eea17b49510a2b7489599688086cd567ea
Rafael Santos <rast.santos@gmail.com>
2021-02-07 20:19:05 -0600

    agregué carpeta js y script para el icono y linkear estilos

---
+++ js/icoAndStyle.js
@@ -0,0 +1,10 @@
+/*linked css*/
+linkCSS = document.createElement("link");
+linkCSS.rel = "stylesheet";
+linkCSS.href = "styles.css";
+document.head.appendChild(linkCSS);
+/*Adding icon*/
+linkIcon = document.createElement("link");
+linkIcon.rel = "shortcut icon";
+linkIcon.href = "assets/logo15.ico";
+document.head.appendChild(linkIcon);
